04bee673903141a2b86b82559cc2a19ebcb38e81
asa-leholland <asaleholland@gmail.com>
2023-06-03 14:27:35 -0700

    feat: :test_tube: Add test cases for missing dirs

--- test_move_and_archive.sh
+++ test_move_and_archive.sh
@@ -81,3 +81,100 @@ echo ""
 # Clean up the test environment
 rm -rf src
 rm -rf tgt
+
+
+
+
+
+# Test case 3: Target directory does not exist
+
+# Log the test case to the terminal
+echo -e "\n~~~Running test case: Target directory does not exist\n"
+mkdir -p src
+touch src/file.txt
+
+# Show the contents of the source and target directories before execution
+echo "PRE-EXECUTION:"
+echo "-Source directory contents:"
+ls src
+echo ""
+
+if [ -d "tgt" ]; then
+  echo "-Target directory contents:"
+  ls tgt
+  echo ""
+else
+  echo "-Target directory does not exist"
+fi
+
+# Run the test case and log any errors to the terminal
+echo "EXECUTING: $script_path src tgt file.txt"
+echo ""
+cmd.exe /c "$script_path src tgt file.txt" 2>&1 | grep -i "error"
+
+# Show the contents of the source and target directories after execution
+echo "POST-EXECUTION:"
+echo "-Source directory contents:"
+ls src
+echo ""
+
+if [ -d "tgt" ]; then
+  echo "-Target directory contents:"
+  ls tgt
+  echo ""
+else
+  echo "-Target directory does not exist"
+fi
+
+# Clean up the test environment
+rm -rf src
+rm -rf tgt
+
+
+
+
+
+
+
+# Test case 4: Source directory does not exist
+
+# Log the test case to the terminal
+echo -e "\n~~~Running test case: Source directory does not exist\n"
+mkdir -p tgt
+
+# Show the contents of the source and target directories before execution
+echo "PRE-EXECUTION:"
+if [ -d "src" ]; then
+  echo "-Source directory contents:"
+  ls src
+  echo ""
+else
+  echo "-Source directory does not exist"
+fi
+
+echo "-Target directory contents:"
+ls tgt
+echo ""
+
+# Run the test case and log any errors to the terminal
+echo "EXECUTING: $script_path src tgt file.txt"
+echo ""
+cmd.exe /c "$script_path src tgt file.txt" 2>&1 | grep -i "error"
+
+# Show the contents of the source and target directories after execution
+echo "POST-EXECUTION:"
+if [ -d "src" ]; then
+  echo "-Source directory contents:"
+  ls src
+  echo ""
+else
+  echo "-Source directory does not exist"
+fi
+
+echo "-Target directory contents:"
+ls tgt
+echo ""
+
+# Clean up the test environment
+rm -rf src
+rm -rf tgt
